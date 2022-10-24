ced3224760828174ee59836960522ae4165d7199
Cleol <cleo.lyoen@gmail.com>
2022-10-24 10:50:30 +0200

    maj exercice

---
+++ script.js
@@ -0,0 +1,41 @@
+let ville = "Cambrai"
+const hypothenuse = 8
+let admission = "vrai"
+let nom = "Lyoen"
+let prenom = "Cleo"
+let âge = 32
+
+let pommes = 4
+let pêche = 6
+let totalFruits =pommes * pêche
+console.log(totalFruits)
+
+let fraises = 10
+let melon = 2
+let Combiendefruits = fraises / melon
+console.log(Combiendefruits)
+
+function addition (a,b) {
+    return a + b
+}
+
+
+/* exercice variable  */
+/*var numero = 33
+var texte = "33"
+let float = 3.14;
+let binaire = true*/
+
+/*let numero = 23
+let texte = "45"
+let totaldesdeux = numero + texte
+console.log(totaldesdeux)*/
+
+let numero = 53
+let texte ="33"
+let temp = "0"
+
+
+
+
+
