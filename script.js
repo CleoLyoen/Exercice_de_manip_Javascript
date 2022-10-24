64f462977e4eb71f31ba7fa6495735ad2aec9311
Cleol <cleo.lyoen@gmail.com>
2022-10-24 15:44:45 +0200

    Maj exercice

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let ville = "Cambrai"
+/*let ville = "Cambrai"
 const hypothenuse = 8
 let admission = "vrai"
 let nom = "Lyoen"
@@ -17,7 +17,7 @@ console.log(Combiendefruits)
 
 function addition (a,b) {
     return a + b
-}
+}*/
 
 
 /* exercice variable  */
@@ -31,9 +31,115 @@ let texte = "45"
 let totaldesdeux = numero + texte
 console.log(totaldesdeux)*/
 
-let numero = 53
+/*let numero = 53 
 let texte ="33"
-let temp = "0"
+var temp = "0"
+
+var temp = parseInt(texte);
+console.log(temp)
+
+let resultat= numero + temp;
+console.log(resultat)*/
+
+
+/*let number = 12;
+let texte = "44";
+
+
+result = parseInt(texte) + number
+console.log(number, ' + ' ,texte, ' = ' ,result);*/
+
+// function isoNum() {
+// return 42;
+// }
+// console.log(isoNum());
+
+// function maFonction(arg1,arg2) {
+//     return arg1 * arg2 + arg1 + arg2;
+// }
+
+// console.log(maFonction(10,1))
+
+// let var1 = "3.14";
+// let nombre = 1 * var1;
+// console.log(nombre)
+
+// let var1 = "mon Texte";
+// let longueur = var1.length;
+// console.log(longueur)
+
+// let var1 = "c'est une ";
+// let var2 = "concatenation";
+// console.log(var1 + var2)
+
+// let var1 = "un anneau pour les trouver";
+// let indice = 1;
+// let resultat = var1[indice];
+// console.log('à la position [',indice,'] il y a le caractère : ' + resultat)
+
+// let var1= "le début est le commencement";
+// let result= var1.indexOf('le');
+// console.log(result)
+
+// let var1= "le javascript c'est bien";
+// let result= var1.replace('javascript', 'code');
+// console.log(result)
+
+// let var1= "lorem ipsum dolor sit amet";
+// let result1= var1.slice();
+// console.log(result1)
+// let result2= var1.slice(var1.indexOf ('d'))
+// console.log(result2)
+
+// let var1= "Lorem ipsum dolor sit amet"
+// let result= var1.substring (var1.indexOf ('d'), var1.indexOf ('t')+ 1);
+// console.log(result)
+
+// let var1= "Lorem ipsum dolor sit amet"
+// let result= var1.slice (var1.indexOf ('d'), var1.indexOf ('t')+ 1);
+// console.log(result)
+
+// let var1 = "tout en minuscule";
+// let result = var1.toUpperCase();
+// console.log(result)
+
+// let var1 = "tout en minuscule";
+// let result = var1.substring (var1.indexOf ('t'), var1.indexOf (' ')+ 1) + 
+// var1.substring (var1.indexOf ('e'), var1.indexOf ('n')+ 1).toUpperCase() +
+// var1.slice (var1.indexOf ('m')-1);
+// console.log(result)
+
+// let result= "l'amiral a dit : 'ceci est inadmissible'";
+// console.log(result)
+
+// let number = 2022;
+// // let result = number.toString();
+// let number2 = 5;
+// console.log(number + number2)
+
+// let result = Math.PI 
+// console.log(result)
+
+// let var1 = 6.7;
+// let result = Math.round(var1)
+// console.log(result)
+
+// let result = Math.floor(Math.random() * 10);
+// console.log(result)
+
+// let var1 = 44
+// let var2 = 34
+// let var3 = 55
+// let var4 = 13
+// let var5 = 17
+
+// let result= Math.max(var1,var2,var3,var4,var5);
+// console.log(result)
+
+
+
+
+
 
 
 
